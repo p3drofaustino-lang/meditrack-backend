8f6ae299b6c849baa4d2d3c9c49a36a61d295f88
PedroFaustino <p3dro.faustino@gmail.com>
2026-05-24 19:07:18 +0100

    chore: initialize backend infrastructure

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send({ message: "MediTrack API is running" });
+app.get('/', (req, res) => {
+  res.send({ message: 'MediTrack API is running' });
 });
 
 app.listen(PORT, () => {
