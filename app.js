ac2e4754170161ad31961dec2152935ed9a5049e
PedroFaustino <p3dro.faustino@gmail.com>
2026-05-24 23:42:43 +0100

    feat: add user model

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/meditrackdb')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
 app.get('/', (req, res) => {
   res.send({ message: 'MediTrack API is running' });
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: 'Invalid email address',
+    },
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
